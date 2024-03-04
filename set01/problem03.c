f18848cdddbb6df289d590dafb4a1cf4566b8b96
shannn9071 <shantharajk.bsc23@rvu.edu.in>
2024-03-04 14:30:39 +0530

    jdd

--- set01/problem03.c
+++ set01/problem03.c
@@ -3,28 +3,27 @@ int input();
 int add(int a, int b);
 void output(int a, int b, int sum);
 
-
-int input(){
+int input() {
     int x;
-    printf("enter a value :");
+    printf("enter a value:");
     scanf("%d",&x);
     return x;
 }
 
 int add(int a, int b) {
-     int sum = a+b;
-     return sum;
+    int sum;
+    sum = a+b;
+    return sum;
+}
+void output(int a, int b, int sum) {
+     printf("the sum of %d and %d is %d", a, b, sum);
 }
 
-void output(int a, int b, int sum) {
-    printf("the sum of two numbers is : %d ",sum);
-}
- int main() {
+int main() {
     int a,b,sum;
-   a = input();
-   b = input();
-    sum = add(a,b);
-   output(a,b,sum);
-    
-    return 0;
- }
+ a = input();
+ b =  input(); 
+sum = add(a, b);
+output( a, b,sum);
+return 0;
+}
--- set01/problem10.c
+++ set01/problem10.c
@@ -1,37 +1,50 @@
 #include <stdio.h>
-
-int main(void) {
-
-int i;
-char ch1[100],ch2[100];
-
-  printf("enter the first string:\n");
-scanf("%s",&ch1);
-
-  printf("enter the second string:\n");
-  scanf("%s",&ch2);
+void input_two_strings(char *string1, char *string2);
+int stringcompare(char *string1, char *string2);
+void output(char *string1, char *string2, int result);
 
 
-for(int i=0 ;ch1[i] == ch2[i]&&ch1[i]!='\0'&&ch2[i]!='\0';i++);
-
-if(ch1[i]>ch2[i])
-{
-printf("%s is greater\n",ch1);
-  return 0;
-  
+void input_two_strings(char *string1, char *string2) {
+    printf("Enter the first string: ");
+    scanf("%s", string1);
+    printf("Enter the second string: ");
+    scanf("%s", string2);
 }
 
-  if(ch2[i]>ch1[i])
-  {
-  printf("%s is greater\n",ch1);
+
+int stringcompare(char *string1, char *string2) {
+    int i = 0;
+    while (string1[i] != '\0' && string2[i] != '\0') {
+        if (string1[i] < string2[i])
+            return -1;
+        else if (string1[i] > string2[i])
+            return 1;
+        i++;
+    }
+    if (string1[i] == '\0' && string2[i] == '\0')
+        return 0;
+    else if (string1[i] == '\0')
+        return -1;
+    else
+        return 1;
+}
+
+void output(char *string1, char *string2, int result) {
+    if (result == 0)
+        printf("Strings \"%s\" and \"%s\" are equal.\n", string1, string2);
+    else if (result < 0)
+        printf("String \"%s\" is less than \"%s\".\n", string1, string2);
+    else
+        printf("String \"%s\" is greater than \"%s\".\n", string1, string2);
+}
+
+int main() {
+    char string1[100], string2[100];
+    int result;
+    
+    input_two_strings(string1, string2);
+    result = stringcompare(string1, string2);
+    output(string1, string2, result);
+    
     return 0;
-
-  }
-
-  else
-  {
-    printf("strings are equal\n");
-  }
-
-  return 0;
 }
